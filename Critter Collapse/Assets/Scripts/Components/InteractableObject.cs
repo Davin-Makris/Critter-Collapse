65ed111b3b6ed89f45f878a79aae4b808005d1dd
mcdonaldnat <nmcdona1@uci.edu>
2026-02-08 15:57:23 -0800

    Added player inventory and holding items- finished

--- Critter Collapse/Assets/Scripts/Components/InteractableObject.cs
+++ Critter Collapse/Assets/Scripts/Components/InteractableObject.cs
@@ -3,7 +3,6 @@ using UnityEngine.Events;
 
 public class InteractableObject : MonoBehaviour, Interactable
 {
-    [SerializeField] private GameObject thisObject; // a reference to this object for adding to inventory
     [SerializeField] private string displayName = "Interact";
     [SerializeField] private bool isEnabled = true;
     [SerializeField] private UnityEvent onInteract;
@@ -22,12 +21,14 @@ public class InteractableObject : MonoBehaviour, Interactable
         outline.OutlineColor = Color.yellow;
         outline.OutlineWidth = 5f;
         outline.enabled = false; // disable it until we focus on it
+
+        lastObject = gameObject; // update to the object this script is attached to
     }
 
     public void Interact()
     {
+        lastObject = gameObject; // update to the object this script is attached to
         onInteract?.Invoke(); //using an event system on On_Interact()
-        lastObject = thisObject;
     }
 
     public void OnFocusGained()
--- Critter Collapse/Assets/Scripts/Inventory.cs
+++ Critter Collapse/Assets/Scripts/Inventory.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {   // you can only  have one thing in the inventoy at a time
     static bool inventoryFull = false;
-    [SerializeField] GameObject inInventory; // the obj currently in the inventory
+    private GameObject inInventory; // the obj currently in the inventory
     [SerializeField] GameObject inventory; // the player's inventory
-    private GameObject lastObject = InteractableObject.lastObject; // the object that we last interacted with
+    //private GameObject lastObject = InteractableObject.lastObject; // the object that we last interacted with
 
 
     public void addToInventory()
@@ -15,9 +15,10 @@ public class Inventory : MonoBehaviour
             inInventory.transform.SetParent(null); // drop the object
             inventoryFull = false;
         }
-        else // otherwise you can pick it up
+        else // otherwise pick it up
         {
-            inInventory.transform.SetParent(inventory.transform); // make the obj a child of the inventory
+            inInventory = InteractableObject.lastObject; // update our inventory
+            inInventory.transform.SetParent(inventory.transform); // pickup the last object we interacted with 
             inventoryFull = true;
         }
     }

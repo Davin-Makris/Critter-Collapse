0f3115c5089a7e70d72484d8bfd7a06a2bfb7c82
mcdonaldnat <nmcdona1@uci.edu>
2026-02-26 18:55:10 -0800

    Finished customer order system

--- Critter Collapse/Assets/Scripts/CustomerOrders/Customer.cs
+++ Critter Collapse/Assets/Scripts/CustomerOrders/Customer.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class Customer : MonoBehaviour
 {
     //FLOWER KEY (how many tiles they take up):
-    // Chocolate Cosmos FLower: 6
-    // Firework Flower: 12
+    // Chocolate Cosmos: 6
+    // Firework: 12
+    // Forget me not: 5
     // Large Lily: 9
     // Lily of the Valley: 7
     // Lotus: 6
@@ -14,42 +16,107 @@ public class Customer : MonoBehaviour
 
     public Dictionary<int, Order> allOrders = new Dictionary<int, Order>(); //a dict of all possible orders
     private Order currentOrder; // a ref to the order we're currently on
-    private GameObject orderText; // a ref to the order text in the canvas for updating
+    [SerializeField] public TMP_Text orderText; // a ref to the order text in the canvas for updating
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //setUpOrders();
+        setUpOrders();
+        orderText.text = "Testing";
     }
 
     // sets all the orders with values for plants, # of plants, and ID
     private void setUpOrders()
     {
-        Dictionary<string, int> orderOne = new Dictionary<string, int>();
+        List<Order> tempOrders = new List<Order>();
+
+        Order orderOne = setOneOrder(1, 0, 2, 1, 1, 1, 1, 1);
+        Order orderTwo = setOneOrder(0, 1, 1, 0, 1, 2, 0, 0);
+        Order orderThree = setOneOrder(0, 0, 2, 1, 2, 1, 0, 0);
+        Order orderFour = setOneOrder(1, 0, 1, 1, 0, 2, 2, 1);
+
+        tempOrders.Add(orderOne);
+        tempOrders.Add(orderTwo);
+        tempOrders.Add(orderThree);
+        tempOrders.Add(orderFour);
+
+        // set all unique int IDs for the orders & add to the dict allOrders for later
+        for (int x = 0; x < tempOrders.Count; x++)
+        {
+            tempOrders[x].setID(x); // set the ID
+            allOrders.Add(x, tempOrders[x]); // add the order to the dict using the ID as the key
+        }
 
     }
 
-    // gets a random incomplete order from allOrders
-    public Order getRandOrder()
+    // sets and returns ONE Order given the param values
+    // params stand for the number of flowers of each type, if there are none the number must be 0, and they must be in order
+    // Order is as defined at the top of this script or in alphabetical order
+    private Order setOneOrder(int choc, int firework, int forgetmenot, int largeLily, int lilyValley, int lotus, int rose, int sun)
     {
-        return new Order();
+        //Order temp = new Order();
+        Order temp = ScriptableObject.CreateInstance<Order>();
+
+        temp.addPlant("Chocolate Cosmo", choc);
+        temp.addPlant("Firework", firework);
+        temp.addPlant("Forget me not", forgetmenot);
+        temp.addPlant("Large Lilie", largeLily);
+        temp.addPlant("Lily of the Valley", lilyValley);
+        temp.addPlant("Lotu", lotus);
+        temp.addPlant("Rose", rose);
+        temp.addPlant("Sunflower", sun);
+
+        return temp; 
+    }
+
+    // gets a random order from the list and updates the order text on New Order button click
+    public void NewOrderButtonClick()
+    {
+        getRandOrder();
+        updateOrderText();
+    }
+
+    // completes the current order
+    public void CompleteOrderButtonClick()
+    {
+        completeOrder(currentOrder.ID);
+    }
+
+    // gets a random incomplete order from allOrders
+    // Random.Range(x, y) gives a range from x (inclusinve) to y (exclusive) aka x to y - 1
+    public void getRandOrder()
+    {
+        bool orderFound = false; // flag check
+
+        foreach (var (id, order) in allOrders)
+        {
+            int randomTemp = Random.Range(0, 4); // 0 - # of orders, randomTemp is the order ID
+                                                 // if it's not already completed, we can use it
+            if (!allOrders[randomTemp].isComplete)
+            {
+                currentOrder = allOrders[randomTemp];
+                orderFound = true;
+                break; // break out of loop since we have what we need
+            }
+        }
+
+        // if we went though all orders but all are complete
+        // either reset all orders to play again or end game
+        if (!orderFound)
+        {
+            orderText.text = "Congrats! You completed all orders! The End :)";
+        }
     }
 
     // updates the orderText in the Canvas
     public void updateOrderText()
     {
-
+        orderText.text = currentOrder.getOrderText();
     }
 
     // marks the order with the given ID as complete
     public void completeOrder(int ID)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        allOrders[ID].setAsComplete(true);
     }
 }
--- Critter Collapse/Assets/Scripts/CustomerOrders/Order.cs
+++ Critter Collapse/Assets/Scripts/CustomerOrders/Order.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Order : MonoBehaviour
+public class Order : ScriptableObject
 {
     public int ID; // the unique ID of the order
     public Dictionary<string, int> orderPlants = new Dictionary<string, int>(); //a dict of the plants and how many are in the order
@@ -11,12 +11,25 @@ public class Order : MonoBehaviour
     public string getOrderText()
     {
         string orderText = "I want:";
+        int step = 0;
 
         foreach (var (plant, count) in orderPlants)
         {
-            if (count == 0) continue;
-            string tempText = $" {count} {plant}s,";
+            string tempText = "";
+
+            if (count == 0) continue; // if there are none of this plant, move to the next iteration
+
+            // if we are at the end
+            if (step == orderPlants.Count - 1)
+            {
+                tempText = $" and {count} {plant}s.";
+            }
+            else
+            {
+                tempText = $" {count} {plant}s,";
+            }
             orderText += tempText;
+
         }
         return orderText;
     }
@@ -30,5 +43,17 @@ public class Order : MonoBehaviour
     public void addPlant(string type, int quantity)
     {
         orderPlants.Add(type, quantity);
+    }
+
+    // sets the ID of this order
+    public void setID(int IDnum)
+    {
+        ID = IDnum;
+    }
+
+    //sets this order as complete (True) or incomplete (False)
+    public void setAsComplete(bool complete)
+    {
+        isComplete = complete; 
     }
 }
